0dc423150e3c61a2b5953c3dd83e3b9b0660d4df
Prajjwall123 <pokhrelprajwal29@gmail.com>
2024-08-10 09:49:56 +0545

    output cleaning

--- Codes/cleaning.r
+++ Codes/cleaning.r
@@ -1,30 +1,63 @@
-# Load necessary libraries
 library(tidyverse)
-library(ggplot2)
 
-# Column names(From 2020 dataset)
-colm_names = c('ID', 'Price', 'Date', 'PostCode', 'null1', 'null2', 'null3', 
-               'POAN', 'SOAN', 'Street', 'Locality', 'Town', 'District', 'County', 'null4', 'null5')
 
-# Read data for each year
-housing20 <- read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2020.csv", col_names = colm_names)
-housing21 <- read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2021.csv", col_names = colm_names)
-housing22 <- read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2022.csv", col_names = colm_names)
-housing23 <- read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2023.csv", col_names = colm_names)
+column_names=c(
+  "ID",
+  "Price",
+  "Date of Transfer",
+  "Postcode",
+  "Property Type",
+  "Old/New",
+  "Duration",
+  "PAON",
+  "SAON",
+  "Street",
+  "Locality",
+  "Town/City",
+  "District",
+  "County",
+  "PPD Category Type"
+)
 
-# Merge datasets
-merged_housing = bind_rows(housing20, housing21, housing22, housing23)
 
-# Clean merged dataset
-final_data <- merged_housing %>%
-  select(-c('null1', 'null2', 'null3', 'null4', 'null5', 'ID', 'SOAN')) %>%
-  filter(grepl("Bristol|Cornwall", County, ignore.case = TRUE)) %>%
-  filter(!is.na(Price))
-  #filter(!is.na(Locality))
-  filter(!is.na("Street"))
+housing20=read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2020.csv",col_names = column_names)
+housing21=read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2021.csv",col_names = column_names)
+housing22=read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2022.csv",col_names = column_names)
+housing23=read_csv("/Users/acer/Desktop/assignment/Obtain/house price/pp-2023.csv",col_names = column_names)
 
-# Specify the path where the cleaned data should be saved
+#View(housing20)
+
+total_housing_data <- bind_rows(housing20, housing21, housing22, housing23)
+
+#View(total_housing_data)
+
+cleaned_data <- total_housing_data %>%
+  select(-`PPD Category Type`, 
+         -`Property Type`, 
+         -`Old/New`, 
+         -`Duration`, 
+         -PAON, 
+         -SAON, 
+         -Street, 
+         -Locality, 
+         -District) %>%
+  filter(!is.na(Price) & 
+           !is.na(`Date of Transfer`) & 
+           !is.na(Postcode) & 
+           !is.na(`Town/City`) & 
+           !is.na(County) & 
+           !is.na(ID))
+
+View(cleaned_data)
+
+filtered_data <- cleaned_data %>%
+  filter(County %in% c('CORNWALL', 'CITY OF BRISTOL'))
+
+#View(filtered_data)
+
 output_path <- "/Users/acer/Desktop/assignment/Cleaned/cleaned_housing_data.csv"
 
-# Write the cleaned data to a CSV file
-write_csv(final_data, output_path)
+write_csv(filtered_data, output_path)#house prices cleaned output csv file
+
+
+  
