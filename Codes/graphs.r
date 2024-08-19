c26d4468d366bdcd894c6e9982252a654d5075bc
Prajjwall123 <pokhrelprajwal29@gmail.com>
2024-08-19 09:15:16 +0545

    robbery pie chart

--- Codes/graphs.r
+++ Codes/graphs.r
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(dplyr)
+library(fmsb)
+library(tidyr)
 
 # Schooling
 schooling_data = read_csv("/Users/acer/Desktop/assignment/Cleaned/cleaned_school.csv")
@@ -113,13 +115,13 @@ bristol_data <- broadband_data %>%
   filter(County == "CITY OF BRISTOL")
 
 
-bristol_summary <- bristol_data %>% 
+bristol_filtered <- bristol_data %>% 
   summarise(Average_Speed = mean(`Average download speed (Mbit/s)`),
             Maximum_Speed = max(`Maximum download speed (Mbit/s)`))
 
 bristol_df <- data.frame(
   Type = c("Average Download Speed", "Maximum Download Speed"),
-  Speed = c(bristol_summary$Average_Speed, bristol_summary$Maximum_Speed)
+  Speed = c(bristol_filtered$Average_Speed, bristol_filtered$Maximum_Speed)
 )
 
 # Bar Chart for Bristol
@@ -130,14 +132,14 @@ ggplot(bristol_df, aes(x = Type, y = Speed)) +
   ylab("Speed (Mbps)")
 
 
-cornwall_summary <- broadband_data %>% 
+cornwall_filtered <- broadband_data %>% 
   filter(County == "CORNWALL") %>%
   summarise(Average_Speed = mean(`Average download speed (Mbit/s)`),
             Maximum_Speed = max(`Maximum download speed (Mbit/s)`))
 
 cornwall_df <- data.frame(
   Type = c("Average Download Speed", "Maximum Download Speed"),
-  Speed = c(cornwall_summary$Average_Speed, cornwall_summary$Maximum_Speed)
+  Speed = c(cornwall_filtered$Average_Speed, cornwall_filtered$Maximum_Speed)
 )
 
 # Bar Chart for Cornwall
@@ -147,4 +149,114 @@ ggplot(cornwall_df, aes(x = Type, y = Speed)) +
   xlab("") +
   ylab("Speed (Mbps)")
 
+
+
+
+
+#Crime
+crime=read_csv("/Users/acer/Desktop/assignment/Cleaned/crime_cleaned.csv")
+
+
+drug_offences = crime %>%
+  filter(`Crime type` == "Drugs")%>%
+  filter(`Year` == "2023")
+
+bristol_drug_offences <- drug_offences %>%
+  filter(counties == "Bristol, City of") %>%
+  summarise(total_population = sum(count),
+            total_drug_offences = n())%>%
+  mutate(offence_rate = (total_drug_offences / total_population) * 10000)
+#View(bristol_drug_offences)
+
+cornwall_drug_offences <- drug_offences %>%
+  filter(counties == "Cornwall") %>%
+  summarise(total_population = sum(count),
+            total_drug_offences = n())%>%
+  mutate(offence_rate = (total_drug_offences / total_population) * 10000)
+#View(cornwall_drug_offences)
+
+combined_data <- bind_rows(
+  cornwall_drug_offences %>% 
+    mutate(county = "Cornwall"),
+  bristol_drug_offences %>% 
+    mutate(county = "Bristol, City of"))
+#View(combined_data)
+
+#Draw the graph
+ggplot(combined_data, aes(x = county, y = offence_rate, fill = county)) +
+  geom_boxplot() +
+  labs(title = "Distribution of Drug Offence Rates (2023)",
+       x = "Location",
+       y = "Offence Rate (per 10,000)") +
+  theme_minimal()
+
+
+#Radar chart of Vehicle Crime Rate From 2021 to 2024
+vehicle_crime = crime %>%
+  filter(`Crime type` == "Vehicle crime")%>%
+  group_by(Year) %>%
+  summarise(total_crime = sum(count, na.rm = TRUE))
+
+vehicle_crime_df <- as.data.frame(t(vehicle_crime$total_crime))
+colnames(vehicle_crime_df) <- vehicle_crime$Year
+
+vehicle_crime_df <- rbind(rep(max(vehicle_crime$total_crime), length(years)), 
+                   rep(0, length(years)), 
+                   vehicle_crime_df)
+
+par(mar = c(2, 2, 2, 2))
+
+radarchart(vehicle_crime_df,
+           pcol = "red",
+           pfcol = "pink",
+           cglcol = "grey",
+           axislabcol = "darkgrey",
+           caxislabels = seq(0, max(vehicle_crime$total_crime)),
+           title = "Vehicle Crime Rate from 2021 to 2024")
+
+
+#crime rate line chart
+crime_data <- read_csv("/Users/acer/Desktop/assignment/Cleaned/crime_cleaned.csv")
+
+county_population <- crime_data %>%
+  select(postcode, count, counties, Year) %>%
+  distinct() %>%
+  group_by(counties, Year) %>%
+  summarize(total_population = sum(count))
+
+drug_offenses <- crime_data %>%
+  filter(`Crime type` == "Drugs") %>%
+  group_by(counties, Year) %>%
+  summarize(total_drug_offenses = n()) %>%
+  na.omit()
+
+drug_rate_per_10k <- left_join(drug_offenses, county_population, by = c("counties", "Year")) %>%
+  mutate(rate_per_10k = (total_drug_offenses / total_population) * 10000)
+
+ggplot(drug_rate_per_10k, aes(x = Year, y = rate_per_10k, color = counties, group = counties)) +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
+  labs(title = "Drug Offense Rate per 10,000 People",
+       x = "Year",
+       y = "Drug Offense Rate per 10,000 People",
+       color = "Counties") +
+  theme_minimal()
+
+
+# robbery  pie chart
+crime_data <- read_csv("/Users/acer/Desktop/assignment/Cleaned/crime_cleaned.csv")
+
+rober_november <- crime_data %>%
+  filter(`Crime type` == "Robbery" & Year == "2023" & Month == "11") %>%
+  group_by(counties) %>%
+  summarise(total_robberies = n()) %>%
+  mutate(percentage = total_robberies / sum(total_robberies) * 100)
+
+ggplot(rober_november, aes(x = "", y = percentage, fill = counties)) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y") +
+  geom_text(aes(label = paste0(round(percentage, 1), "%")), 
+            position = position_stack(vjust = 0.5)) +
+  labs(title = "Distribution of Robberies by County (November 2023)", fill = "County") +
+  theme_minimal()
 
